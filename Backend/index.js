4b78360bf80cf070a919e0e367bd08d2065ed534
mohammad ayan <mohammadayan2210@gmail.com>
2026-09-07 23:53:36 +0530

    Update CORS configuration and add start script to package.json

--- Backend/index.js
+++ Backend/index.js
@@ -27,7 +27,7 @@ import adminUserRoutes from "./routes/admin/adminUser.routes.js";
 const app = express();
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:5174", "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_VENDOR, process.env.FRONTEND_URL_ADMIN],
     credentials: true
 }));
 
